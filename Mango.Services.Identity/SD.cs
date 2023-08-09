4f3ab12bfe8aae83985c55abd3f0cac94ede0f70
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2023-08-08 21:51:54 -0600

    setting up Identity Server

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,62 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = nameof(Admin);
+        public const string Customer = nameof(Customer);
+
+        /// <summary>
+        /// things that we want to protect in our IDs
+        /// </summary>
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+
+        /// <summary>
+        /// identifiers for resources that the CLIENT want to access
+        /// there are two types of scopes:
+        /// - Identity Scope: could contain FirstName, LastName, Profile, etc
+        /// - Resource Scope: 
+        /// </summary>
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("mango", "Mango Server"),
+                new ApiScope("read", "Read Db Data"),
+                new ApiScope("write", "Write into Db"),
+                new ApiScope("delete", "Delete from Db"),
+            };
+
+        /// <summary>
+        /// CLIENT piece of software that request the token from IDs
+        /// it can be for authenticating user or for accessing resource
+        /// example: web app, mobile app, etc.
+        /// </summary>
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "MangoWeb",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "mango"
+                    },
+                    RedirectUris = { "https://localhost:7253/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:7253/signout-callback-oidc" }
+                }
+            };
+    }
+}
--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -3,10 +3,10 @@
     public class ProductDto
     {
         public int ProductId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public double Price { get; set; }
-        public string Description { get; set; }
-        public string CategoryName { get; set; }
-        public string ImageUrl { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
