6ffb12c053cd9727120d77aa0a18b4b221cd4fce
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-08-16 18:09:43 -0600

    IdentityServer memory configuration is up and running

--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -1,10 +1,44 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
+configuration.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configuring IdentityServer
+services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+
+    options.EmitStaticAudienceClaim = true;
+    // SD = Standard
+}).AddInMemoryIdentityResources(SD.IdentityResources) // Identity Resources
+    .AddInMemoryApiScopes(SD.ApiScopes) // Identity Api Scopes
+    .AddInMemoryClients(SD.Clients) // Clients
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddDeveloperSigningCredential();
+        
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +53,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
